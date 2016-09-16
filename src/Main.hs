90fba69ff15784247cfa1065d101464f9a6cbb6f
rkoeninger <rkoeninger@att.net>
2016-09-15 22:26:24 -0400

    Initial commit with some demo opengl code

---
+++ src/Main.hs
@@ -0,0 +1,42 @@
+import Graphics.UI.GLUT
+ 
+myPoints :: [(GLfloat,GLfloat,GLfloat)]
+myPoints = [ (sin (2*pi*k/64), cos (2*pi*k/64), 0) | k <- [1..64] ]
+ 
+main :: IO ()
+main = do
+  (_progName, _args) <- getArgsAndInitialize
+  _window <- createWindow "Hello World"
+  displayCallback $= display
+  mainLoop
+ 
+display :: DisplayCallback
+display = do
+  let color3f r g b = color $ Color3 r g (b :: GLfloat)
+      vertex3f x y z = vertex $ Vertex3 x y (z :: GLfloat)
+  clear [ColorBuffer]
+  renderPrimitive Quads $ do
+    color3f 1 0 0
+    vertex3f 0 0 0
+    vertex3f 0 0.2 0
+    vertex3f 0.2 0.2 0
+    vertex3f 0.2 0 0
+ 
+    color3f 0 1 0
+    vertex3f 0 0 0
+    vertex3f 0 (-0.2) 0
+    vertex3f 0.2 (-0.2) 0
+    vertex3f 0.2 0 0
+ 
+    color3f 0 0 1
+    vertex3f 0 0 0
+    vertex3f 0 (-0.2) 0
+    vertex3f (-0.2) (-0.2) 0
+    vertex3f (-0.2) 0 0
+ 
+    color3f 1 0 1
+    vertex3f 0 0 0
+    vertex3f 0 0.2 0
+    vertex3f (-0.2) 0.2 0
+    vertex3f (-0.2) 0 0
+  flush
