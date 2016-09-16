460eb5d75c2315115e305245cf154da158eadbeb
rkoeninger <rkoeninger@att.net>
2016-09-15 23:45:59 -0400

    Removed annoying extra spaces

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,15 @@
 import Graphics.UI.GLUT
- 
+
 myPoints :: [(GLfloat,GLfloat,GLfloat)]
 myPoints = [ (sin (2*pi*k/64), cos (2*pi*k/64), 0) | k <- [1..64] ]
- 
+
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
   _window <- createWindow "Hello World"
   displayCallback $= display
   mainLoop
- 
+
 display :: DisplayCallback
 display = do
   let color3f r g b = color $ Color3 r g (b :: GLfloat)
@@ -21,19 +21,19 @@ display = do
     vertex3f 0 0.2 0
     vertex3f 0.2 0.2 0
     vertex3f 0.2 0 0
- 
+
     color3f 0 1 0
     vertex3f 0 0 0
     vertex3f 0 (-0.2) 0
     vertex3f 0.2 (-0.2) 0
     vertex3f 0.2 0 0
- 
+
     color3f 0 0 1
     vertex3f 0 0 0
     vertex3f 0 (-0.2) 0
     vertex3f (-0.2) (-0.2) 0
     vertex3f (-0.2) 0 0
- 
+
     color3f 1 0 1
     vertex3f 0 0 0
     vertex3f 0 0.2 0
