971176afd42f243642f431d8c8d7629392238fd4
rkoeninger <rkoeninger@att.net>
2016-10-11 13:47:10 -0400

    Replaced ad-hoc test with hspec

--- test/Test.hs
+++ test/Test.hs
@@ -1,24 +1,23 @@
 module Main where
 
-import System.Exit (exitSuccess, exitFailure)
+import Test.Hspec
 import Famihask
 import Memory
 
-testStoreWord = do
-	ram <- malloc 2
-	storeWord ram 0 0x8cf3
-	b0 <- load ram 0
-	b1 <- load ram 1
-	return $ (b0 == 0xf3) && (b1 == 0x8c)
+main :: IO ()
+main = hspec $ do
+    describe "Memory" $ do
+        it "words should be stored little-endian" $ do
+            ram <- malloc 2
+            storeWord ram 0 0x8cf3
+            b0 <- load ram 0
+            b1 <- load ram 1
+            b0 `shouldBe` 0xf3
+            b1 `shouldBe` 0x8c
 
-testLoadWord = do
-	ram <- malloc 2
-	store ram 0 0xf3
-	store ram 1 0x8c
-	w <- loadWord ram 0
-	return $ w == 0x8cf3
-
-main = do
-	result0 <- testStoreWord
-	result1 <- testLoadWord
-	if result0 && result1 then exitSuccess else exitFailure
+        it "words should be loaded little-endian" $ do
+            ram <- malloc 2
+            store ram 0 0xf3
+            store ram 1 0x8c
+            w <- loadWord ram 0
+            w `shouldBe` 0x8cf3
