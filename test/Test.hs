54e7c3f116d17c008aa581ab76b27a76be5ff00d
Robert Koeninger <rkoeninger@att.net>
2016-11-18 16:32:39 -0500

    Cleaned up flag assertions

--- test/Test.hs
+++ test/Test.hs
@@ -15,14 +15,10 @@ infixl 1 |>
 
 aRegIs state val = it "accumulator should be" $ aReg state `shouldBe` val
 
-negativeFlagSet   state = it "negative flag should be set"   $ negativeFlag state `shouldBe` True
-negativeFlagClear state = it "negative flag should be clear" $ negativeFlag state `shouldBe` False
-overflowFlagSet   state = it "overflow flag should be set"   $ overflowFlag state `shouldBe` True
-overflowFlagClear state = it "overflow flag should be clear" $ overflowFlag state `shouldBe` False
-zeroFlagSet       state = it "zero flag should be set"       $ zeroFlag     state `shouldBe` True
-zeroFlagClear     state = it "zero flag should be clear"     $ zeroFlag     state `shouldBe` False
-carryFlagSet      state = it "carry flag should be set"      $ carryFlag    state `shouldBe` True
-carryFlagClear    state = it "carry flag should be clear"    $ carryFlag    state `shouldBe` False
+negativeFlagIs x state = it ("negative flag should be " ++ show x) $ negativeFlag state `shouldBe` x
+overflowFlagIs x state = it ("overflow flag should be " ++ show x) $ overflowFlag state `shouldBe` x
+zeroFlagIs     x state = it ("zero flag should be "     ++ show x) $ zeroFlag     state `shouldBe` x
+carryFlagIs    x state = it ("carry flag should be "    ++ show x) $ carryFlag    state `shouldBe` x
 
 testROM = describe "ROM" $
     it "rom file header should begin with magic" $ do
@@ -69,66 +65,66 @@ testArithmetic = describe "Arithmetic" $ do
     context "should perform simple, non-carried, non-overflow, addition" $ do
         let state = arithmeticScenario 21 33 False (+)
         aRegIs state 54
-        zeroFlagClear state
-        negativeFlagClear state
-        overflowFlagClear state
-        carryFlagClear state
+        zeroFlagIs False state
+        negativeFlagIs False state
+        overflowFlagIs False state
+        carryFlagIs False state
 
     context "should perform simple, previous carry, non-overflow, addition" $ do
         let state = arithmeticScenario 21 33 True (+)
         aRegIs state 55
-        zeroFlagClear state
-        negativeFlagClear state
-        overflowFlagClear state
-        carryFlagClear state
+        zeroFlagIs False state
+        negativeFlagIs False state
+        overflowFlagIs False state
+        carryFlagIs False state
 
     context "should perform simple, non-carried, overflowing, addition resulting in carry" $ do
         let state = arithmeticScenario 150 150 False (+)
         aRegIs state 44
-        zeroFlagClear state
-        negativeFlagClear state
-        overflowFlagSet state
-        carryFlagSet state
+        zeroFlagIs False state
+        negativeFlagIs False state
+        overflowFlagIs True state
+        carryFlagIs True state
 
     context "should perform simple, previous carry, overflowing, addition resulting in carry" $ do
         let state = arithmeticScenario 150 150 True (+)
         aRegIs state 45
-        zeroFlagClear state
-        negativeFlagClear state
-        overflowFlagSet state
-        carryFlagSet state
+        zeroFlagIs False state
+        negativeFlagIs False state
+        overflowFlagIs True state
+        carryFlagIs True state
 
     context "should perform simple, non-carried, non-overflow, subtraction" $ do
         let state = arithmeticScenario 49 31 False (-)
         aRegIs state 17
-        zeroFlagClear state
-        negativeFlagClear state
-        overflowFlagClear state
-        carryFlagSet state
+        zeroFlagIs False state
+        negativeFlagIs False state
+        overflowFlagIs False state
+        carryFlagIs True state
 
     context "should perform simple, previous carry, non-overflow, subtraction" $ do
         let state = arithmeticScenario 49 31 True (-)
         aRegIs state 18
-        zeroFlagClear state
-        negativeFlagClear state
-        overflowFlagClear state
-        carryFlagSet state
+        zeroFlagIs False state
+        negativeFlagIs False state
+        overflowFlagIs False state
+        carryFlagIs True state
 
     context "should perform simple, non-carried, overflowing, subtraction resulting in a carry" $ do
         let state = arithmeticScenario 49 81 False (-)
         aRegIs state 223
-        zeroFlagClear state
-        negativeFlagSet state
-        overflowFlagClear state
-        carryFlagClear state
+        zeroFlagIs False state
+        negativeFlagIs True state
+        overflowFlagIs False state
+        carryFlagIs False state
 
     context "should perform simple, previous carry, overflowing, subtraction resulting in a carry" $ do
         let state = arithmeticScenario 49 81 True (-)
         aRegIs state 224
-        zeroFlagClear state
-        negativeFlagSet state
-        overflowFlagClear state
-        carryFlagClear state
+        zeroFlagIs False state
+        negativeFlagIs True state
+        overflowFlagIs False state
+        carryFlagIs False state
 
 compareScenario a val = do
     defaultState
@@ -142,21 +138,21 @@ compareScenario a val = do
 testComparisons = describe "Comparisons" $ do
     context "first argument is greater than second" $ do
         let state = compareScenario 1 0
-        zeroFlagClear state
-        negativeFlagClear state
-        carryFlagClear state
+        zeroFlagIs False state
+        negativeFlagIs False state
+        carryFlagIs False state
 
     context "first argument is less than second" $ do
         let state = compareScenario 0 1
-        zeroFlagClear state
-        negativeFlagSet state
-        carryFlagSet state
+        zeroFlagIs False state
+        negativeFlagIs True state
+        carryFlagIs True state
 
     context "first argument is equal to second" $ do
         let state = compareScenario 0 0
-        zeroFlagSet state
-        negativeFlagClear state
-        carryFlagClear state
+        zeroFlagIs True state
+        negativeFlagIs False state
+        carryFlagIs False state
 
 rotateScenario a carry lr = do
     let opCode = case lr () of
@@ -173,44 +169,44 @@ testRotate = describe "Rotate" $ do
     context "when carry bit is clear, rotating left should leave right most bit clear" $ do
         let state = rotateScenario 0xff False Left
         aRegIs state 0xfe
-        carryFlagSet state
-        zeroFlagClear state
-        negativeFlagSet state
+        carryFlagIs True state
+        zeroFlagIs False state
+        negativeFlagIs True state
 
     context "when carry bit is set, rotating left should leave right most bit set" $ do
         let state = rotateScenario 0xff True Left
         aRegIs state 0xff
-        carryFlagSet state
-        zeroFlagClear state
-        negativeFlagSet state
+        carryFlagIs True state
+        zeroFlagIs False state
+        negativeFlagIs True state
 
     context "when carry bit is clear, rotating right should leave left most bit clear" $ do
         let state = rotateScenario 0xff False Right
         aRegIs state 0x7f
-        carryFlagSet state
-        zeroFlagClear state
-        negativeFlagClear state
+        carryFlagIs True state
+        zeroFlagIs False state
+        negativeFlagIs False state
 
     context "when carry bit is set, rotating right should leave left most bit set" $ do
         let state = rotateScenario 0xff True Right
         aRegIs state 0xff
-        carryFlagSet state
-        zeroFlagClear state
-        negativeFlagSet state
+        carryFlagIs True state
+        zeroFlagIs False state
+        negativeFlagIs True state
 
     context "when left most bit is clear, rotating left should clear carry bit" $ do
         let state = rotateScenario 0x7f False Left
         aRegIs state 0xfe
-        carryFlagClear state
-        zeroFlagClear state
-        negativeFlagSet state
+        carryFlagIs False state
+        zeroFlagIs False state
+        negativeFlagIs True state
 
     context "when right most bit is clear, rotating right should clear carry bit" $ do
         let state = rotateScenario 0xfe False Right
         aRegIs state 0x7f
-        carryFlagClear state
-        zeroFlagClear state
-        negativeFlagClear state
+        carryFlagIs False state
+        zeroFlagIs False state
+        negativeFlagIs False state
 
 main :: IO () 
 main = hspec $ do
