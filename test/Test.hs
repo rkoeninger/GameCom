de799d3468f1adf37ffc833cd90f49fd23c67baf
rkoeninger <rkoeninger@att.net>
2016-11-17 20:14:15 -0500

    Refactored tests

--- test/Test.hs
+++ test/Test.hs
@@ -13,7 +13,7 @@ x |> f = f x
 
 infixl 1 |>
 
-aRegShouldBe state val = it "accumulator should be" $ aReg state `shouldBe` val
+aRegIs state val = it "accumulator should be" $ aReg state `shouldBe` val
 
 negativeFlagSet   state = it "negative flag should be set"   $ negativeFlag state `shouldBe` True
 negativeFlagClear state = it "negative flag should be clear" $ negativeFlag state `shouldBe` False
@@ -24,165 +24,153 @@ zeroFlagClear     state = it "zero flag should be clear"     $ zeroFlag     stat
 carryFlagSet      state = it "carry flag should be set"      $ carryFlag    state `shouldBe` True
 carryFlagClear    state = it "carry flag should be clear"    $ carryFlag    state `shouldBe` False
 
+testMemory = describe "Memory" $ do
+    it "words should be stored little-endian" $ do
+        let state = storeWord 0 0x8cf3 defaultState
+        loadByte 0 state `shouldBe` 0xf3
+        loadByte 1 state `shouldBe` 0x8c
+
+    it "words should be loaded little-endian" $ do
+        let state = storeByte 1 0x8c $ storeByte 0 0xf3 defaultState
+        loadWord 0 state `shouldBe` 0x8cf3
+
+testArithmetic = describe "Arithmetic" $ do
+    context "should perform simple, non-carried, non-overflow, addition" $ do
+        let state = defaultState
+                    |> setAReg 21 -- argument
+                    |> storeByte 0x00 33 -- other argument
+                    |> storeByte 0x10 0x65 -- adc/zpg instruction
+                    |> storeByte 0x11 0x00 -- zpg address
+                    |> setPCReg 0x0010 -- set PC to location of adc/zpg
+                    |> CPU.step
+        aRegIs state 54
+        zeroFlagClear state
+        negativeFlagClear state
+        overflowFlagClear state
+        carryFlagClear state
+
+    context "should perform simple, previous carry, non-overflow, addition" $ do
+        let state = defaultState
+                    |> setAReg 21 -- argument
+                    |> setCarryFlag True
+                    |> storeByte 0x00 33 -- other argument
+                    |> storeByte 0x10 0x65 -- adc/zpg instruction
+                    |> storeByte 0x11 0x00 -- zpg address
+                    |> setPCReg 0x0010 -- set PC to location of adc/zpg
+                    |> CPU.step
+        aRegIs state 55
+        zeroFlagClear state
+        negativeFlagClear state
+        overflowFlagClear state
+        carryFlagClear state
+
+    context "should perform simple, non-carried, overflowing, addition resulting in carry" $ do
+        let state = defaultState
+                    |> setAReg 150 -- argument
+                    |> storeByte 0x00 150 -- other argument
+                    |> storeByte 0x10 0x65 -- adc/zpg instruction
+                    |> storeByte 0x11 0x00 -- zpg address
+                    |> setPCReg 0x0010 -- set PC to location of adc/zpg
+                    |> CPU.step
+        aRegIs state 44
+        zeroFlagClear state
+        negativeFlagClear state
+        overflowFlagSet state
+        carryFlagSet state
+
+    context "should perform simple, previous carry, overflowing, addition resulting in carry" $ do
+        let state = defaultState
+                    |> setAReg 150 -- argument
+                    |> setCarryFlag True
+                    |> storeByte 0x00 150 -- other argument
+                    |> storeByte 0x10 0x65 -- adc/zpg instruction
+                    |> storeByte 0x11 0x00 -- zpg address
+                    |> setPCReg 0x0010 -- set PC to location of adc/zpg
+                    |> CPU.step
+        aRegIs state 45
+        zeroFlagClear state
+        negativeFlagClear state
+        overflowFlagSet state
+        carryFlagSet state
+
+    context "should perform simple, non-carried, non-overflow, subtraction" $ do
+        let state = defaultState
+                    |> setAReg 49 -- argument
+                    |> storeByte 0x00 31 -- other argument
+                    |> storeByte 0x10 0xe5 -- sbc/zpg instruction
+                    |> storeByte 0x11 0x00 -- zpg address
+                    |> setPCReg 0x0010 -- set PC to location of sbc/zpg
+                    |> CPU.step
+        aRegIs state 17
+        zeroFlagClear state
+        negativeFlagClear state
+        overflowFlagSet state
+        carryFlagSet state
+
+    context "should perform simple, previous carry, non-overflow, subtraction" $ do
+        let state = defaultState
+                    |> setAReg 49 -- argument
+                    |> setCarryFlag True
+                    |> storeByte 0x00 31 -- other argument
+                    |> storeByte 0x10 0xe5 -- sbc/zpg instruction
+                    |> storeByte 0x11 0x00 -- zpg address
+                    |> setPCReg 0x0010 -- set PC to location of sbc/zpg
+                    |> CPU.step
+        aRegIs state 18
+        zeroFlagClear state
+        negativeFlagClear state
+        overflowFlagSet state
+        carryFlagSet state
+
+    context "should perform simple, non-carried, overflowing, subtraction resulting in a carry" $ do
+        let state = defaultState
+                    |> setAReg 49 -- argument
+                    |> storeByte 0x00 81 -- other argument
+                    |> storeByte 0x10 0xe5 -- sbc/zpg instruction
+                    |> storeByte 0x11 0x00 -- zpg address
+                    |> setPCReg 0x0010 -- set PC to location of sbc/zpg
+                    |> CPU.step
+        aRegIs state 223
+        zeroFlagClear state
+        negativeFlagSet state
+        overflowFlagClear state
+        carryFlagClear state
+
+    context "should perform simple, previous carry, overflowing, subtraction resulting in a carry" $ do
+        let state = defaultState
+                    |> setAReg 49 -- argument
+                    |> setCarryFlag True
+                    |> storeByte 0x00 81 -- other argument
+                    |> storeByte 0x10 0xe5 -- sbc/zpg instruction
+                    |> storeByte 0x11 0x00 -- zpg address
+                    |> setPCReg 0x0010 -- set PC to location of sbc/zpg
+                    |> CPU.step
+        aRegIs state 224
+        zeroFlagClear state
+        negativeFlagSet state
+        overflowFlagClear state
+        carryFlagClear state
+
+testROM = describe "ROM" $
+    it "rom file header should begin with magic" $ do
+        let bytes = [0x4e, 0x45, 0x53, 0x1a] ++ replicate 12 0x00
+        let rom = ROM {
+            mirroring = Horizontal,
+            trainer = False,
+            persistent = False,
+            inesMapper = 0,
+            mapper = 0,
+            playChoice = False,
+            unisystem = False,
+            ramSize = 0,
+            region = NTSC,
+            prg = B.empty,
+            chr = B.empty
+        }
+        parseROM (B.pack bytes) `shouldBe` Right rom
+
 main :: IO ()
 main = hspec $ do
-    describe "Memory" $ do
-        it "words should be stored little-endian" $ do
-            let state = storeWord 0 0x8cf3 defaultState
-            loadByte 0 state `shouldBe` 0xf3
-            loadByte 1 state `shouldBe` 0x8c
-
-        it "words should be loaded little-endian" $ do
-            let state = storeByte 1 0x8c $ storeByte 0 0xf3 defaultState
-            loadWord 0 state `shouldBe` 0x8cf3
-
-    describe "CPU" $ do
-        context "negative numbers should set the negative flag in the status register" $ do
-            let state = setZN 0x80 defaultState
-            negativeFlagSet state
-            zeroFlagClear state
-
-        context "zero should set the zero flag in the status register" $ do
-            let state = setZN 0x00 defaultState
-            negativeFlagClear state
-            zeroFlagSet state
-
-        context "positive numbers should not set the zero or negative flags in the status register" $ do
-            let state = setZN 0x01 defaultState
-            negativeFlagClear state
-            zeroFlagClear state
-
-    describe "ROM" $
-        it "rom file header should begin with magic" $ do
-            let bytes = [0x4e, 0x45, 0x53, 0x1a] ++ replicate 12 0x00
-            let rom = ROM {
-                mirroring = Horizontal,
-                trainer = False,
-                persistent = False,
-                inesMapper = 0,
-                mapper = 0,
-                playChoice = False,
-                unisystem = False,
-                ramSize = 0,
-                region = NTSC,
-                prg = B.empty,
-                chr = B.empty
-            }
-            parseROM (B.pack bytes) `shouldBe` Right rom
-
-    describe "Arithmetic" $ do
-        context "should perform simple, non-carried, non-overflow, addition" $ do
-            let state = defaultState
-                        |> setAReg 21 -- argument
-                        |> storeByte 0x00 33 -- other argument
-                        |> storeByte 0x10 0x65 -- adc/zpg instruction
-                        |> storeByte 0x11 0x00 -- zpg address
-                        |> setPCReg 0x0010 -- set PC to location of adc/zpg
-                        |> CPU.step
-            aRegShouldBe state 54
-            zeroFlagClear state
-            negativeFlagClear state
-            overflowFlagClear state
-            carryFlagClear state
-
-        context "should perform simple, previous carry, non-overflow, addition" $ do
-            let state = defaultState
-                        |> setAReg 21 -- argument
-                        |> setCarryFlag True
-                        |> storeByte 0x00 33 -- other argument
-                        |> storeByte 0x10 0x65 -- adc/zpg instruction
-                        |> storeByte 0x11 0x00 -- zpg address
-                        |> setPCReg 0x0010 -- set PC to location of adc/zpg
-                        |> CPU.step
-            aRegShouldBe state 55
-            zeroFlagClear state
-            negativeFlagClear state
-            overflowFlagClear state
-            carryFlagClear state
-
-        context "should perform simple, non-carried, overflowing, addition resulting in carry" $ do
-            let state = defaultState
-                        |> setAReg 150 -- argument
-                        |> storeByte 0x00 150 -- other argument
-                        |> storeByte 0x10 0x65 -- adc/zpg instruction
-                        |> storeByte 0x11 0x00 -- zpg address
-                        |> setPCReg 0x0010 -- set PC to location of adc/zpg
-                        |> CPU.step
-            aRegShouldBe state 44
-            zeroFlagClear state
-            negativeFlagClear state
-            overflowFlagSet state
-            carryFlagSet state
-
-        context "should perform simple, previous carry, overflowing, addition resulting in carry" $ do
-            let state = defaultState
-                        |> setAReg 150 -- argument
-                        |> setCarryFlag True
-                        |> storeByte 0x00 150 -- other argument
-                        |> storeByte 0x10 0x65 -- adc/zpg instruction
-                        |> storeByte 0x11 0x00 -- zpg address
-                        |> setPCReg 0x0010 -- set PC to location of adc/zpg
-                        |> CPU.step
-            aRegShouldBe state 45
-            zeroFlagClear state
-            negativeFlagClear state
-            overflowFlagSet state
-            carryFlagSet state
-
-        context "should perform simple, non-carried, non-overflow, subtraction" $ do
-            let state = defaultState
-                        |> setAReg 49 -- argument
-                        |> storeByte 0x00 31 -- other argument
-                        |> storeByte 0x10 0xe5 -- sbc/zpg instruction
-                        |> storeByte 0x11 0x00 -- zpg address
-                        |> setPCReg 0x0010 -- set PC to location of sbc/zpg
-                        |> CPU.step
-            aRegShouldBe state 17
-            zeroFlagClear state
-            negativeFlagClear state
-            overflowFlagSet state
-            carryFlagSet state
-
-        context "should perform simple, previous carry, non-overflow, subtraction" $ do
-            let state = defaultState
-                        |> setAReg 49 -- argument
-                        |> setCarryFlag True
-                        |> storeByte 0x00 31 -- other argument
-                        |> storeByte 0x10 0xe5 -- sbc/zpg instruction
-                        |> storeByte 0x11 0x00 -- zpg address
-                        |> setPCReg 0x0010 -- set PC to location of sbc/zpg
-                        |> CPU.step
-            aRegShouldBe state 18
-            zeroFlagClear state
-            negativeFlagClear state
-            overflowFlagSet state
-            carryFlagSet state
-
-        context "should perform simple, non-carried, overflowing, subtraction resulting in a carry" $ do
-            let state = defaultState
-                        |> setAReg 49 -- argument
-                        |> storeByte 0x00 81 -- other argument
-                        |> storeByte 0x10 0xe5 -- sbc/zpg instruction
-                        |> storeByte 0x11 0x00 -- zpg address
-                        |> setPCReg 0x0010 -- set PC to location of sbc/zpg
-                        |> CPU.step
-            aRegShouldBe state 223
-            zeroFlagClear state
-            negativeFlagSet state
-            overflowFlagClear state
-            carryFlagClear state
-
-        context "should perform simple, previous carry, overflowing, subtraction resulting in a carry" $ do
-            let state = defaultState
-                        |> setAReg 49 -- argument
-                        |> setCarryFlag True
-                        |> storeByte 0x00 81 -- other argument
-                        |> storeByte 0x10 0xe5 -- sbc/zpg instruction
-                        |> storeByte 0x11 0x00 -- zpg address
-                        |> setPCReg 0x0010 -- set PC to location of sbc/zpg
-                        |> CPU.step
-            aRegShouldBe state 224
-            zeroFlagClear state
-            negativeFlagSet state
-            overflowFlagClear state
-            carryFlagClear state
+    testMemory
+    testArithmetic
+    testROM
