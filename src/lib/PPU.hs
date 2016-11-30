cea0cffd8ff8bce0fe7745e9168b5482868f3279
rkoeninger <rkoeninger@att.net>
2016-11-30 00:27:03 -0500

    Added getSpriteInfo to PPU
    Started getSpritePixel in PPU

--- src/lib/PPU.hs
+++ src/lib/PPU.hs
@@ -63,8 +63,8 @@ inBoundingBox sprite x y state =
 tiles :: Sprite -> MachineState -> SpriteTile
 tiles sprite state =
     case spriteSize state of
-    Size8x8  -> Tile8x8 $ byteToWord i .|. base
-    Size8x16 -> Tile8x16 first $ first + 1
+    Size8x8  -> Tile8x8 (byteToWord i .|. base)
+    Size8x16 -> Tile8x16 first (first + 1)
     where base = spritePatternTableAddr state
           i = tileIndex sprite
           addend = if testBit i 0 then 0x1000 else 0
@@ -86,17 +86,6 @@ nextScanline runToCycle state =
 step :: MachineState -> MachineState
 step state = nextScanline (cyclesPerScanline + cycleCount state) state
 
--- Returns the color (pre-palette lookup) of pixel (x,y) within the given tile.
-getPatternPixel :: PixelLayer -> Word16 -> (Word8, Word8) -> MachineState -> (Word8, MachineState)
-getPatternPixel layer tile (x, y) state = do
-    let offset = (tile `shiftL` 4) + byteToWord y + patternTableAddr layer state
-    let (plane0, stat2) = loadVramByte offset state
-    let (plane1, stat3) = loadVramByte (offset + 8) stat2
-    let amount = 7 - (x `mod` 8)
-    let bit0 = (plane0 `shiftR` fromIntegral amount) .&. 0x01
-    let bit1 = (plane1 `shiftR` fromIntegral amount) .&. 0x01
-    ((bit1 `shiftL` 1) .|. bit0, stat3)
-
 nametableAddr :: Word16 -> Word16 -> NametableAddress
 nametableAddr x y = do
     let xIndex = x `mod` 64
@@ -112,16 +101,45 @@ nametableAddr x y = do
         yIndex = wordToByte (yIndex `mod` 30)
     }
 
+getSpriteInfo :: Word16 -> MachineState -> Sprite
+getSpriteInfo index state =
+    Sprite {
+        yPosition = at (index * 4 + 0) (oamData state) + 1,
+        tileIndex = at (index * 4 + 1) (oamData state),
+        attribute = at (index * 4 + 2) (oamData state),
+        xPosition = at (index * 4 + 3) (oamData state)
+    }
+
+--mapSprites :: (Sprite -> MachineState -> (MachineState, Bool)) -> MachineState
+--mapSprites f = () [0 .. 63]
+
+{-  fn each_sprite<F>(&mut self, mut f: F) where F: FnMut(&mut Ppu, &SpriteStruct, u8) -> bool{
+        for i in 0..64
+            let sprite = self.make_sprite_info(i as u16)
+            if !f(self, &sprite, i as u8)
+                return                        -}
+
+-- Returns the color (pre-palette lookup) of pixel (x,y) within the given tile.
+getPatternPixel :: PixelLayer -> Word16 -> (Word8, Word8) -> MachineState -> (Word8, MachineState)
+getPatternPixel layer tile (x, y) state = do
+    let offset = (tile `shiftL` 4) + byteToWord y + patternTableAddr layer state
+    let (plane0, stat2) = loadVramByte offset state
+    let (plane1, stat3) = loadVramByte (offset + 8) stat2
+    let amount = 7 - (x `mod` 8)
+    let bit0 = (plane0 `shiftR` fromIntegral amount) .&. 0x01
+    let bit1 = (plane1 `shiftR` fromIntegral amount) .&. 0x01
+    ((bit1 `shiftL` 1) .|. bit0, stat3)
+
 getBackgroundPixel :: Word8 -> MachineState -> (Maybe Color, MachineState)
 getBackgroundPixel x state = do
     let x2 = byteToWord x + byteToWord (ppuScrollX state)
     let y2 = byteToWord (scanline state) + byteToWord (ppuScrollY state)
     let nt = nametableAddr (x2 `shiftR` 3) (y2 `shiftR` 3)
     let ntBase = base nt
-    let ntXIndex = byteToWord $ xIndex nt
-    let ntYIndex = byteToWord $ yIndex nt
-    let xSub = wordToByte $ x2 .&. 0x07
-    let ySub = wordToByte $ y2 .&. 0x07
+    let ntXIndex = byteToWord (xIndex nt)
+    let ntYIndex = byteToWord (yIndex nt)
+    let xSub = wordToByte (x2 .&. 0x07)
+    let ySub = wordToByte (y2 .&. 0x07)
     let (tile, stat2) = loadVramByte (ntBase + (ntXIndex `shiftL` 5) + ntYIndex) state
     let (patternColor, stat3) = getPatternPixel BackgroundLayer (byteToWord tile) (xSub, ySub) stat2
     if patternColor == 0 then
@@ -139,3 +157,50 @@ getBackgroundPixel x state = do
         let tileColor = (attrTableColor `shiftL` 2) .|. patternColor
         let (paletteIndex, stat5) = loadVramByte (0x3f00 + byteToWord tileColor) stat4
         (Just(colors !! fromIntegral (paletteIndex .&. 0x3f)), stat5)
+
+getSpritePixel :: [Maybe Word8] -> Word8 -> Bool -> MachineState -> Maybe SpriteColor
+getSpritePixel [] _ _ _ = Nothing
+getSpritePixel (Nothing : _) _ _ _ = Nothing
+getSpritePixel (Just spriteIndex : rest) x opaqueBackground state = do
+    let sprite = getSpriteInfo (byteToWord spriteIndex) state
+    if inBoundingBox sprite x (scanline state) state then
+        getSpritePixel rest x opaqueBackground state
+    else
+        case tiles sprite state of -- TODO: 8x16 not implemented
+        Tile8x8 tile -> Nothing
+        Tile8x16 tile _ -> Nothing
+{-
+let pattern_color;
+match sprite.tiles(self) {
+    // TODO: 8x16 rendering
+    SpriteTiles8x8(tile) | SpriteTiles8x16(tile, _) => {
+        let mut x = x - sprite.x;
+        if sprite.flip_horizontal() { x = 7 - x; }
+
+        let mut y = self.scanline as u8 - sprite.y;
+        if sprite.flip_vertical() { y = 7 - y; }
+
+        debug_assert!(x < 8, "sprite X miscalculation");
+        debug_assert!(y < 8, "sprite Y miscalculation");
+
+        pattern_color = self.get_pattern_pixel(PatternPixelKind::Sprite, tile, x, y);
+    }
+}
+
+// If the pattern color was zero, this part of the sprite is transparent.
+if pattern_color == 0 {
+    continue
+}
+
+// OK, so we know this pixel is opaque. Now if this is the first sprite and the
+// background was not transparent, set sprite 0 hit.
+if index == 0 && background_opaque {
+    self.regs.status.set_sprite_zero_hit(true);
+}
+
+// Determine final tile color and do the palette lookup.
+let tile_color = (sprite.palette() << 2) | pattern_color;
+let palette_index = self.vram.loadb(0x3f00 + (tile_color as u16)) & 0x3f;
+let final_color = self.get_color(palette_index);
+
+return Some(SpriteColor { priority: sprite.priority(), color: final_color });    -}
