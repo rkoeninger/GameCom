4dcde203a9dc8f9e44642efdca92108fe2f647f5
Robert Koeninger <rkoeninger@att.net>
2017-05-10 10:38:02 -0400

    Added mkdir to fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -4,8 +4,9 @@ SDL_VERSION=2.0.3
 FILE_NAME=SDL2-$SDL_VERSION.tar.gz
 
 wget -q http://www.libsdl.org/release/$FILE_NAME
+mkdir -p libsdl2
 tar xf $FILE_NAME -C libsdl2
-rm 
+rm $FILE_NAME
 cd libsdl2
 ./configure
 make
