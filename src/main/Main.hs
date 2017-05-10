11b5e0b8e10bfd413a6ddc623e800a31e068bc09
Robert Koeninger <rkoeninger@att.net>
2017-05-09 23:04:16 -0400

    Added sdl2 as dependency and sdl demo code to Main

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -1,3 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-main = putStrLn "Hi"
+import Control.Concurrent (threadDelay)
+import Foreign.C.Types
+import SDL.Vect
+import qualified SDL
+
+screenWidth, screenHeight :: CInt
+(screenWidth, screenHeight) = (640, 480)
+
+main = do
+    SDL.initialize [SDL.InitVideo]
+
+    window <- SDL.createWindow "SDL Tutorial" SDL.defaultWindow { SDL.windowInitialSize = V2 screenWidth screenHeight }
+    SDL.showWindow window
+
+    screenSurface <- SDL.getWindowSurface window
+    let white = V4 maxBound maxBound maxBound maxBound
+    SDL.surfaceFillRect screenSurface Nothing white
+    SDL.updateWindowSurface window
+
+    threadDelay 2000000
+
+    SDL.destroyWindow window
+    SDL.quit
+
