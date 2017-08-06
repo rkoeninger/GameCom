cd51348b8cbe8962d62fa4e7bbed8be2054cb189
Robert Koeninger <rkoeninger@att.net>
2017-08-06 00:48:42 -0400

    Removed fetch.*, sdl2.pc.windows

--- fetch.bat
+++
@@ -1,5 +0,0 @@
-set SDL_VERSION=2.0.5
-set FILE_NAME=SDL2-%SDL_VERSION%-win32-x64.zip
-powershell.exe -Command "Invoke-WebRequest -Uri https://www.libsdl.org/release/%FILE_NAME% -OutFile %FILE_NAME%"
-powershell.exe -Command "Expand-Archive %FILE_NAME% -DestinationPath libsdl2 -Force"
-del %FILE_NAME%
